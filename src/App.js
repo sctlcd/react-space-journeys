627bd92bc1aaab1126d6c2a0ff328514fb6f0409
sctlcd <lucinda.souchet@gmail.com>
2025-01-14 21:49:24 +0100

    created moon component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 import Button from './components/Button';
 import SpaceShipObject from './objects/SpaceShipObject';
 import EarthObject from './objects/EarthObject';
+import MoonObject from './objects/MoonObject';
 // import Ship from './images/space_ship_3.png';
 import Earth from './images/earth.png';
+import Moon from './images/moon.png';
 
 function App() {
   return (
@@ -14,9 +16,9 @@ function App() {
           btnClass={"btn-danger"} 
         />
         <section className="env">
-          <SpaceShipObject scale={10}/>
-          <EarthObject earth={Earth}/>
-          {/* <MoonObject /> */}
+          <SpaceShipObject scale={10} />
+          <EarthObject earth={Earth} />
+          <MoonObject moon={Moon} />
           
         </section>
         <Button 
---
+++ src/objects/MoonObject.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MoonObject({moon}) {
+  return (
+    <img src={moon} alt="moon" className="moon"/>
+  )
+}
