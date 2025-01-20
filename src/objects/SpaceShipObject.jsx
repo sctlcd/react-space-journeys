2925961dd2509f2fa35cdcac4be329886a5a550d
sctlcd <lucinda.souchet@gmail.com>
2025-01-20 17:33:56 +0100

    cleaned up

--- src/objects/SpaceShipObject.jsx
+++ src/objects/SpaceShipObject.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import ship from "./../images/space_ships/space_ship_1.png";
 
 export default function SpaceShipObject({ changeShip, scale, launch}) {
   let animation = launch;
